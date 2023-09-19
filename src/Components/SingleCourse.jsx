d5549cb55d8282af810d13b0140c721483d90892
Dashka_W <Daskendrik@users.noreply.github.com>
2023-09-19 12:55:46 +0300

    add info course

--- src/Components/SingleCourse.jsx
+++ src/Components/SingleCourse.jsx
@@ -1,5 +1,18 @@
+import { Link, useParams } from 'react-router-dom';
+import courses from '../Data/courses';
 const SingleCourse = () => {
-  return <h1>SingleCourse</h1>;
+  const params = useParams();
+  const course = courses.find((course) => course.slug === params.slug);
+  return (
+    <>
+      <h1>SingleCourse</h1>
+      <h3>{course.title}</h3>
+      <h3>{course.id}</h3>
+      <Link to=".." relative="path">
+        all courses
+      </Link>
+    </>
+  );
 };
 
 export default SingleCourse;
